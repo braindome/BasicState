8d7354b6ec9a0fd272ade41bd2da8d832bd8e1b2
Antonio Saracino <antonio.saracino83@gmail.com>
2023-06-25 04:24:30 +0200

    second

--- app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
+++ app/src/main/java/com/example/basicstatecodelab/WaterCounter.kt
@@ -15,6 +15,31 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 
+
+@Composable
+fun StatelessCounter(count: Int, onIncrement: () -> Unit, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        if (count < 10) {
+            Text("You've had $count glasses")
+        }
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add one")
+        }
+    }
+
+}
+
+@Composable
+fun StatefulCounter(modifier: Modifier = Modifier) {
+    var waterCount by rememberSaveable { mutableStateOf(0) }
+    var juiceCount by rememberSaveable { mutableStateOf(0) }
+
+    StatelessCounter(waterCount, { waterCount++ }, modifier)
+    StatelessCounter(juiceCount, { juiceCount++ }, modifier)
+
+}
+
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
